6424a40f815484eb24bd8bf9b4c1c78b4d5a512f
hoangphihiep <phihiep31012004@gmail.com>
2024-11-19 13:34:57 +0700

    project test spring boot

---
+++ test_springboot/src/main/java/vn/iotstar/TestSpringbootApplication.java
@@ -0,0 +1,15 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class TestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringbootApplication.class, args);
+	}
+}
